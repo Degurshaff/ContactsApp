6a349ba353f6986be5764bc24fd427a90b60b119
Degurshaff <ilyaexe666@gmail.com>
2022-03-09 19:27:32 +0700

    Создана бизнес логика

---
+++ ContactsApp/ContactsApp/Model/Phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ContactsApp.Model
+{
+   public class Phone 
+    {
+        private long _number;
+        public long Number
+        {
+            get => _number;
+            set
+            {
+                if(value.ToString().Length !=11 || value.ToString()[0] != '7')
+                {
+                    throw new ArgumentException(message: "Номер телефона начинается с 7 и 11 состоять из чифр ");
+                }
+                _number = value;
+
+            }
+        }
+    }
+}
---
+++ ContactsApp/ContactsApp/Project.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ContactsApp.Model;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// основа. Класс с основными данными проекта 
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// список контактов
+        /// </summary>
+        public List<Contact> Contacts { get; } = new List<Contact>();
+        /// <summary>
+        /// сортировка в алфавитном порядке
+        /// </summary>
+        /// <returns>Возвращает отсортированый по фамилии в алфавитном порядке список контактов</returns>
+        public List<Contact> GetSorted() => Contacts.OrderBy(i => i.LastName).ToList();
+        /// <summary>
+        /// поиск по фамилии
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>Возвращает отсортированый по фамилии в алфавитном порядку начиная с заданной строки</returns>
+        public List<Contact> FindSorted(strinq query) => Contacts
+        .Where(i => i.LastName.StartsWith(query))
+        .OrderBy(j => j.LastName)
+        .ToList();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Equals(Project other)
+        {
+            return Contacts.SequenceEqual(other.Contacts);
+        }
+
+    }
+}
