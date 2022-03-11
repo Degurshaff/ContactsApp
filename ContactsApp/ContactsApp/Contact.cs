7a82835a92770f9f67205d25fbe157a08aeb8045
Degurshaff <ilyaexe666@gmail.com>
2022-03-11 19:35:15 +0700

    1

--- ContactsApp/ContactsApp/Contact.cs
+++ ContactsApp/ContactsApp/Contact.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ContactsApp.Model
+namespace ContactsApp
 {
     /// <summary>
     /// содержит информацию о контакте 
--- ContactsApp/ContactsApp/Phone.cs
+++ ContactsApp/ContactsApp/Phone.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
-namespace ContactsApp.Model
+namespace ContactsApp
 {
    public class Phone 
     {
